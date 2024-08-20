838934782f326feeef2e801b364b84b7f9b4504e
mansi <mnayak@bestpeers.com>
2024-08-20 14:08:50 +0530

    register the models

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,52 @@
+# from django.contrib import admin
+# from .models import Category, User, Product, OrderItem, Profile, Order, Cart, CartItem
+
+# # Register your models here.
+
+
+# @admin.register(Category)
+# class CategoryAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'name', 'description')
+
+
+# @admin.register(User)
+# class UserAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'email', 'user_type', 'email_verified', 
+#                     'verification_code', 'is_approved')
+
+
+# @admin.register(Profile)
+# class ProfileAdmin(admin.ModelAdmin):
+#     list_dispaly = ('id', 'user', 'phone_number', 'address_line_1',
+#                     'address_line_2', 'city', 'state', 'postal_code',
+#                     'country')
+
+
+# @admin.register(Product)
+# class ProductAdmin(admin.ModelAdmin):
+#     list_dispaly = ('id', 'name', 'category', 'seller',
+#                     'description', 'price', 'stock', 'size', 'image')
+    
+
+# @admin.register(Order)
+# class OrderAdmin(admin.ModelAdmin):
+#     list_dispaly = ('id', 'order', 'product', 'price', 'quantity')
+
+
+# @admin.register(OrderItem)
+# class OrderItemAdmin(admin.ModelAdmin):
+#     list_dispaly = ('product', 'price', 'quantity')
+
+
+# @admin.register(Cart)
+# class CartAdmin(admin.ModelAdmin):
+#     list_dispaly = ('id', 'buyer')
+
+
+# @admin.register(CartItem)
+# class CartItemAdmin(admin.ModelAdmin):
+#     list_dispaly = ('id', 'cart', 'product', 'quantity')
+
 from django.contrib import admin
 from .models import Category, User, Product, OrderItem, Profile, Order, Cart, CartItem
 
@@ -6,7 +55,7 @@ from .models import Category, User, Product, OrderItem, Profile, Order, Cart, Ca
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'description')
+    list_display = ('id', 'category_name', 'description', 'cat_image')
 
 
 @admin.register(User)
@@ -24,7 +73,7 @@ class ProfileAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_dispaly = ('id', 'name', 'category', 'seller',
+    list_dispaly = ('id', 'product_name', 'category', 'seller',
                     'description', 'price', 'stock', 'size', 'image')
     
 
