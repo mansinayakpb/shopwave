2c25222e78692f4e0ba4eeced00c40ff92923db2
mansi <mnayak@bestpeers.com>
2024-08-20 14:27:42 +0530

    modify the urls

--- shop/urls.py
+++ shop/urls.py
@@ -1,18 +1,20 @@
 from django.urls import path
-from .views import Home, StoreView
+from .views import Home, StoreView, ProductView
 from django.conf.urls.static import static
 from django.conf import settings
-
-
 
 urlpatterns = [    
     path('', Home.as_view(), name='home'),
     path('store/', StoreView.as_view(), name='storeview'),
+    path('store/<str:category_name>/', StoreView.as_view(), name='product_by_category_name'),
+    path('store/<str:category_name>/product-detail/<str:product_name>/', ProductView.as_view(), name='product_by_category'),
+    
+    
+    
 ]
+
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:  
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
