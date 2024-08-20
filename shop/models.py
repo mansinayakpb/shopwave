d8863c41049302418ef3704c7cbd0af7bf352117
mansi <mnayak@bestpeers.com>
2024-08-20 14:03:25 +0530

    updated the models

--- shop/models.py
+++ shop/models.py
@@ -42,8 +42,8 @@ class Profile(TimesStampedModel):
     user = models.OneToOneField(User, on_delete=models.CASCADE, 
                                 related_name='profile')
     phone_number = models.CharField(max_length=20, blank=True)
-    # profile_picture = models.ImageField(upload_to='profile_pictures/', 
-    #                                     blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', 
+                                        blank=True)
     # address fields
     address_line_1 = models.CharField(max_length=255, blank=True, null=True)
     address_line_2 = models.CharField(max_length=255, blank=True, null=True)
@@ -58,11 +58,12 @@ class Profile(TimesStampedModel):
 
 class Category(TimesStampedModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=255, unique=True)
+    category_name = models.CharField(max_length=255, unique=True)
+    cat_image = models.ImageField(upload_to='static/', null=True, blank=True)
     description = models.TextField(blank=True)
 
     def __str__(self):
-        return self.name
+        return self.category_name
 
 
 class Product(TimesStampedModel):
@@ -78,15 +79,15 @@ class Product(TimesStampedModel):
                                related_name='seller_products')
     category = models.ForeignKey('Category', on_delete=models.CASCADE, 
                                  null=True, blank=True, related_name='category_products')
-    name = models.CharField(max_length=255)
+    product_name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
     size = models.CharField(max_length=10, choices=SIZE_CHOICES, default='M')
-    image = models.ImageField(upload_to='static/', null=True, blank=True) 
+    image = models.ImageField(upload_to='static/', null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.product_name
 
 
 class Order(TimesStampedModel):
