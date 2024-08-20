f15c40f1d091dff1ba747b9371d995da38ee3013
mansi <mnayak@bestpeers.com>
2024-08-20 14:26:16 +0530

    views are created

--- shop/views.py
+++ shop/views.py
@@ -4,19 +4,36 @@ from .models import Category, Product
 from .forms import CategoryForm, ProductForm
 
 
+# class Home(TemplateView):
+#     template_name = "home.html"
+
+#     def get(self, request):
+#         return render(request, self.template_name)
+
+
 class Home(TemplateView):
-    template_name = "home.html"
+    template_name = 'home.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+        categories = Category.objects.all()  # Fetch all categories from the database
+        context = {'categories': categories}
+        return render(request, self.template_name, context)
 
 
 class StoreView(TemplateView):
     template_name = 'stores/store.html'
 
-    def get(self, request):
+    def get(self, request, category_name=None):
         categories = Category.objects.all()
         products = Product.objects.all()
+
+        if category_name:
+            try:
+                category = Category.objects.get(category_name=category_name)
+                products = Product.objects.filter(category=category)
+            except Category.DoesNotExist:
+                products = Product.objects.none()  
+
         context = {
             'categories': categories,
             'products': products
@@ -24,101 +41,5 @@ class StoreView(TemplateView):
         return render(request, self.template_name, context)
 
 
-class CreateCategory(TemplateView):
-    template_name = ''
-
-    def get(self, request):
-        form = CategoryForm()
-        return render(request, self.template_name, {'form': form})
-
-    def post(self, request):
-        form = CategoryForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("")
-        else:
-            form = CategoryForm()
-        return render(request, self.template_name, {"form": form})
-    
-
-class UpdateCategory(TemplateView):
-    template_name = ''
-
-    def get(self, request, pk):
-        category = Category.objects.filter(id=pk).first()
-        if category:
-            form = CategoryForm(instance=category)
-            return render(request, self.template_name, {'form': form})
-        # else:
-        #     # return HttpResponse("")
-
-    def post(self, request, pk):
-        category = Category.objects.filter(id=pk).first()
-        if category:
-            form = CategoryForm(request.POST, instance=category)
-            if form.is_valid():
-                form.save()
-                return render(request, self.template_name, {'form': form})
-            else:
-                return render(request, self.template_name, {'form': form})
-            
-
-class ViewCategory(TemplateView):
-    template_name = ''
-
-    def get(self, request, pk):
-        category = Category.objects.filter(id=pk).first()
-        if category:
-            return render(request, self.template_name, {"category": category})
-        # else:
-        #     # return HttpResponse("Post not found.")
-
-
-# class DeleteCategory(TemplateView):
-#     template_name = ''
-
-#     def post(self, request, pk):
-#         category = Category.objects.get(id=pk)
-#         if category:
-#             category.delete()
-#             return redirect('')
-#         return render(request, self.template_name, {'category': category})
-    
-
-class CreateProduct(TemplateView):
-    template_name = ''
-
-    def get(self, request):
-        form = ProductForm()
-        return render(request, self.template_name, {'form': form})
-
-    def post(self, request):
-        form = ProductForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("")
-        else:
-            form = ProductForm()
-        return render(request, self.template_name, {"form": form})
-    
-
-class UpdateProduct(TemplateView):
-    template_name = ''
-
-    def get(self, request, pk):
-        product = Product.objects.filter(id=pk).first()
-        if product:
-            form = ProductForm(instance=product)
-            return render(request, self.template_name, {'form': form})
-        # else:
-        #     # return HttpResponse("")
-
-    def post(self, request, pk):
-        product = Product.objects.filter(id=pk).first()
-        if product:
-            form = ProductForm(request.POST, instance=product)
-            if form.is_valid():
-                form.save()
-                return render(request, self.template_name, {'form': form})
-            else:
-                return render(request, self.template_name, {'form': form})
+class ProductView(TemplateView):
+    template_name = 'product-detail/product.html'
