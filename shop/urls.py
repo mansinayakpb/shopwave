49e480277635bd668f1e3ab8ecffad9c639c597f
mansi <mnayak@bestpeers.com>
2024-08-22 11:31:59 +0530

    update the urls

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Home, StoreView, ProductView, CartView
+from .views import Home, StoreView, ProductView, CartView, SignUpView
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -9,9 +9,17 @@ urlpatterns = [
     path('store/<str:category_name>/', StoreView.as_view(), name='product_by_category_name'),
     path('store/<str:category_name>/product-detail/<str:product_name>/', ProductView.as_view(), name='product_by_category'),
     path('product/<str:product_name>', ProductView.as_view(), name='product_detail'),
-    path('cart/', CartView.as_view(), name='cart_view')
-
+    path('cart/', CartView.as_view(), name='cart_view'),
+    path('signin/', SignUpView.as_view(), name='signin'),
+   
+    # path('store/<uuid:pk>/', StoreView.as_view(), name='product_by_category_name'),
+    # path('store/<uuid:pk>/product-detail/<uuid:product_pk>/', ProductView.as_view(), name='product_by_category'),
+    # path('product/<uuid:product_pk>/', ProductView.as_view(), name='product_detail'),
 ]
+
+
+
+
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
