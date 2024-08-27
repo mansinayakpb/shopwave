da4564579f7ea0884fcc99f87fc21c08b5be9fe7
mansi <mnayak@bestpeers.com>
2024-08-27 10:32:38 +0530

    views are updated

--- shop/views.py
+++ shop/views.py
@@ -1,18 +1,11 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, View
-from .models import Category, Product, Cart, CartItem
-from .forms import SignUpForm
+from .models import Category, Product, Cart, CartItem, Profile
+from .forms import SignUpForm, BuyerForm
 from django.contrib import messages
+from django.utils import timezone
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
-
-# from django.views import View
-
-# class Home(TemplateView):
-#     template_name = "home.html"
-
-#     def get(self, request):
-#         return render(request, self.template_name)
 
 
 class Home(TemplateView):
@@ -35,7 +28,6 @@ class StoreView(TemplateView):
         category_name = request.GET.get('category')
         
         if category_name:
-            # Ensure we're matching with the correct field name `category_name`
             category = Category.objects.filter(category_name=category_name).first()
             products = Product.objects.filter(category=category) 
         else:
@@ -68,28 +60,115 @@ class CartView(TemplateView):
         cart = Cart.objects.filter(buyer=request.user).first()
         cart_items = CartItem.objects.filter(cart=cart)
 
+        total_price = 0.0
+        tax_rate = 0.10
+        for item in cart_items:
+            item.total_price = float(item.product.price) * float(item.quantity)
+            total_price += item.total_price
+
+        tax = total_price * tax_rate
+        final_total = total_price + tax
+
         context = {
             'cart': cart,
             'cart_items': cart_items,
+            'total_price': total_price,
+            'tax': tax,
+            'final_total': final_total,
         }
         return render(request, self.template_name, context)
+    
+
+class AddToCartView(View):
+    def post(self, request):
+        product_id = request.POST.get('product_id')
+        product = Product.objects.filter(id=product_id).first()
+
+        if product:
+            cart = Cart.objects.filter(buyer=request.user).first()
+            if not cart:
+                cart = Cart(buyer=request.user)
+                cart.save()
+
+            cart_item = CartItem.objects.filter(cart=cart, product=product).first()
+            if not cart_item:
+                cart_item = CartItem(cart=cart, product=product, quantity=1)
+                cart_item.save()
+            else:
+                cart_item.quantity += 1
+                cart_item.save()
+
+        return redirect('cart_view')
+
+
+class QuantityView(TemplateView):
+    def post(self, request):
+        cart_item_id = request.POST.get('cart_item_id')
+        quantity_action = request.POST.get('quantity_action')
+
+        cart_item = CartItem.objects.filter(id=cart_item_id).first()
+
+        if cart_item:
+            if quantity_action == 'increase':
+                cart_item.quantity += 1
+            elif quantity_action == 'decrease':
+                if cart_item.quantity > 1:
+                    cart_item.quantity -= 1
+
+            cart_item.save()
+
+        return redirect('cart_view')
+    
+
+class RemoveView(TemplateView):
+    def post(self, request, item_id):
+        item = CartItem.objects.filter(id=item_id).first()
+
+        if item:
+            item.delete()
+            messages.success(request, 'item removed successfully')
+        else:
+            messages.error(request, 'item does not exist')
+
+        return redirect('cart_view')
+
+
+
+
+
+
+
+
+        
+
+        
+
+
+       
+
+
+
+
+
+
+
+
+
 
 # SIGNUP
-
 
 class SignUpView(TemplateView):
     template_name = 'signin/register.html'
 
-    def get(self, request, *args, **kwargs):
-        # Render the registration form
+    def get(self, request):
         if request.user.is_authenticated:
             return redirect('/')
         form = SignUpForm()
         return render(request, self.template_name, {'form': form})
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         if request.user.is_authenticated:
-            return redirect('/')
+            return redirect('/') 
 
         form = SignUpForm(request.POST)
         if form.is_valid():
@@ -97,7 +176,6 @@ class SignUpView(TemplateView):
             messages.success(request, "Account Created Successfully!")
             return redirect('/')
         
-        # Re-render the form with validation errors if form is not valid
         return render(request, self.template_name, {'form': form})
 
 # Login
@@ -132,15 +210,32 @@ class UserLoginView(TemplateView):
 # profile
 
 
+# class UserProfileView(TemplateView):
+#     template_name = 'signin/profile.html'
+
+#     def get(self, request):
+#         if request.user.is_authenticated:
+#             return render(request, self.template_name, {'name': request.user})
+#         else:
+#             return redirect('/login/')
+
 class UserProfileView(TemplateView):
     template_name = 'signin/profile.html'
 
     def get(self, request):
-        if request.user.is_authenticated:
-            return render(request, self.template_name, {'name': request.user})
-        else:
-            return redirect('/login/')
+        user_profile = Profile.objects.filter(user=request.user).first()
+        if not user_profile:
+            user_profile = Profile(user=request.user)
+            user_profile.save()
+        
+        form = BuyerForm(instance=user_profile)
+        context = {
+            'form': form
+           
+        }
+        return render(request, self.template_name, context)
 
+    
 
 # logout
 
@@ -149,5 +244,6 @@ class UserLogoutView(View):
     def get(self, request):
         logout(request)
         return redirect('/')
+
 
 
