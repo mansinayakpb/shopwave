69b6ea35642c7212f53545249902a5c622199c5c
Himanshu <himanshughodse@gmail.com>
2024-08-30 19:41:28 +0530

    created urls

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Home, StoreView, ProductView, CartView, AddToCartView, QuantityView, RemoveView, OrderView
+from .views import Home, StoreView, ProductView, CartView, AddToCartView, QuantityView, RemoveView, OrderView, PaymentProcessView, PaymentSuccess, PaymentCancel
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -12,8 +12,15 @@ urlpatterns = [
     path('cart/quantity/', QuantityView.as_view(), name='quantity_view'),
     path('cart/remove/<uuid:item_id>/', RemoveView.as_view(), name='remove_from_cart'),
     path('cart/placeorder/', OrderView.as_view(), name='order_place'),
+    path('payment/process/', PaymentProcessView.as_view(), name='payment_process'),
+    # path('success/', success_view, name='success_page'),
+    # path('cancel/', cancel_view, name='cancel_page'),
+
+    path("payment-success/", PaymentSuccess, name="payment-success"),
+    path("payment-cancel/", PaymentCancel, name="payment-cancel"),
+
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-if settings.DEBUG:  
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
