0f6be9598357946ce3a4d9daa10d384618598c88
mansi <mnayak@bestpeers.com>
2024-08-30 16:32:29 +0530

    update the urls

--- shop/urls.py
+++ shop/urls.py
@@ -1,22 +1,35 @@
 from django.urls import path
-from .views import Home, StoreView, ProductView, CartView, AddToCartView, QuantityView, RemoveView, OrderView
+from .views import (
+    Home,
+    StoreView,
+    ProductView,
+    CartView,
+    AddToCartView,
+    QuantityView,
+    RemoveView,
+    OrderView,
+)
 from django.conf.urls.static import static
 from django.conf import settings
 
-urlpatterns = [    
-    path('', Home.as_view(), name='home'),
-    path('store/', StoreView.as_view(), name='store'),
-    path('product/<uuid:pk>/', ProductView.as_view(), name='product'),
-    path('cart/', CartView.as_view(), name='cart_view'),
-    path('addtocart/', AddToCartView.as_view(), name='add_to_cart'),
-    path('cart/quantity/', QuantityView.as_view(), name='quantity_view'),
-    path('cart/remove/<uuid:item_id>/', RemoveView.as_view(), name='remove_from_cart'),
-    path('cart/placeorder/', OrderView.as_view(), name='order_place'),
-
-    
+urlpatterns = [
+    path("", Home.as_view(), name="home"),
+    path("store/", StoreView.as_view(), name="store"),
+    path("product/<uuid:pk>/", ProductView.as_view(), name="product"),
+    path("cart/", CartView.as_view(), name="cart_view"),
+    path("addtocart/", AddToCartView.as_view(), name="add_to_cart"),
+    path("cart/quantity/", QuantityView.as_view(), name="quantity_view"),
+    path(
+        "cart/remove/<uuid:item_id>/",
+        RemoveView.as_view(),
+        name="remove_from_cart",
+    ),
+    path("cart/placeorder/", OrderView.as_view(), name="order_place"),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-if settings.DEBUG:  
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
