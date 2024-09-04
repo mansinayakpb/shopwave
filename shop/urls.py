621c24ee73178ae3a6ee6bb39f9361bd51fee47c
mansi <mnayak@bestpeers.com>
2024-09-04 16:23:23 +0530

    add the application urls

--- shop/urls.py
+++ shop/urls.py
@@ -1,16 +1,36 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
+
 from .views import (
-    Home,
-    StoreView,
-    ProductView,
-    CartView,
+    ActivateAccountView,
     AddToCartView,
+    BuyerDashboardView,
+    BuyerProfileView,
+    CartView,
+    ChangePasswordView,
+    CreateProductBySellerView,
+    Home,
+    OrderCompleteView,
+    OrderHistoryView,
+    OrderView,
+    ProductView,
     QuantityView,
     RemoveView,
-    OrderView,
+    ResetCompleteView,
+    ResetConfirmView,
+    ResetDoneView,
+    ResetPasswordView,
+    SellerDashboardView,
+    SellerProductsView,
+    SellerProfileView,
+    SignUpView,
+    StoreView,
+    SuccessView,
+    UpdateSellerProductView,
+    UserLoginView,
+    UserLogoutView,
 )
-from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns = [
     path("", Home.as_view(), name="home"),
@@ -25,6 +45,55 @@ urlpatterns = [
         name="remove_from_cart",
     ),
     path("cart/placeorder/", OrderView.as_view(), name="order_place"),
+    # path("success/", SuccessView.as_view(), name="success"),
+    path("OrderComplete/", OrderCompleteView.as_view(), name="Order_Complete"),
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path("login/", UserLoginView.as_view(), name="login"),
+    path("logout/", UserLogoutView.as_view(), name="logout"),
+    path("buyer/profile/", BuyerProfileView.as_view(), name="buyer_profile"),
+    path("OrderHistory/", OrderHistoryView.as_view(), name="Order_History"),
+    path(
+        "buyer/dashboard/",
+        BuyerDashboardView.as_view(),
+        name="buyer_dashboard",
+    ),
+    path(
+        "seller/profile/", SellerProfileView.as_view(), name="seller_profile"
+    ),
+    path(
+        "seller/dashboard/",
+        SellerDashboardView.as_view(),
+        name="seller_dashboard",
+    ),
+    path(
+        "seller/create-product/",
+        CreateProductBySellerView.as_view(),
+        name="createproduct_seller",
+    ),
+    path(
+        "my-products/", SellerProductsView.as_view(), name="myproducts_seller"
+    ),
+    path(
+        "edit-product/<uuid:product_id>/",
+        UpdateSellerProductView.as_view(),
+        name="updateproduct_seller",
+    ),
+    path("success/", SuccessView.as_view(), name="success"),
+    # email verification
+    path(
+        "activate/<str:uid>/<str:token>/",
+        ActivateAccountView.as_view(),
+        name="activate",
+    ),
+    # Forgot password
+    path("passwordreset/", ResetPasswordView.as_view(), name="password_reset"),
+    path("resetdone/", ResetDoneView.as_view(), name="reset_done"),
+    path("resetconfirm/", ResetConfirmView.as_view(), name="reset_confirm"),
+    path("resetcomplete/", ResetCompleteView.as_view(), name="reset_complete"),
+    # Change password
+    path(
+        "changepassword/", ChangePasswordView.as_view(), name="change_password"
+    ),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
