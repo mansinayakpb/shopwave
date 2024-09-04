89cec92a7536144f41713389cd06a9115053e80a
mansi <mnayak@bestpeers.com>
2024-09-04 18:36:13 +0530

    models are created

--- shop/models.py
+++ shop/models.py
@@ -1,8 +1,10 @@
 import uuid
+
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
+
 from shop.managers import UserManager
 
 
